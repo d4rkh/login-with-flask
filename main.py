ab733d7ce762c1cdf04cc7c02292ea54ba0753ae
Madhav Purohit <foritprotv@gmail.com>
2021-02-05 11:57:41 +0530

    new file:   database.json 	new file:   login-failed.html 	new file:   login-success.html 	new file:   login.html 	new file:   main.py 	new file:   share.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+login_page = open('login.html', 'rb').read()
+login_success = open('login-success.html', 'rb').read()
+login_failed = open('login-failed.html', 'rb').read()
+
+@app.route('/', methods=['GET'])
+def home():
+    return login_page
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    database = open('database.json', 'rb').read()
+    database = eval(database)
+
+    username = request.form['username']
+    password = request.form['password']
+
+    for i in database[0]['data']:
+        if i['username'] == username:
+            if i['password'] == password:
+                return login_success, 200
+            
+    return login_failed, 403
+
+app.run(host="0.0.0.0")
